944bdc3e9619c9d9b9158623c2af045c81be5429
Sergey Vatagin <s.vatagin@gmail.com>
2016-04-02 11:48:12 +0300

    add Catalog component

---
+++ app/components/catalog/Catalog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Catalog extends Component {
+
+  render() {
+
+    return (
+        <div className="catalog-wrapper">
+          <h1>Catalog</h1>
+        </div>
+    )
+  }
+}
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import classNames from 'classnames'
+import classNames from 'classnames';
 
-import { testUi } from '../actions/ui'
+import { testUi } from '../actions/ui';
+
+import Catalog from '../components/catalog/Catalog';
 
 
 class App extends Component {
 
   componentDidMount() {
-    this.props.dispatch(testUi())
+    this.props.dispatch(testUi());
   }
 
   render() {
 
     return (
 
-      <div id="ts_main">
-        <h1>Hello Unicat</h1>
+      <div id="ts_main" className="container">
+        <Catalog />
       </div>
 
     )
@@ -24,7 +26,7 @@ class App extends Component {
 }
 
 function mapStateToProps(state) {
-  const { ui, cities, weather} = state
+  const { ui, cities, weather} = state;
 
   return {
     ui,
